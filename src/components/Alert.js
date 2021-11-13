c7b6546b9cd9b09bf1277dd60ccf6baf902f2a37
Saiyam Masalia <masaliasaiyam97@gmail.com>
2021-11-13 13:56:50 +0530

    First Commit

---
+++ src/components/Alert.js
@@ -0,0 +1,30 @@
+Alright so here we start with our topic which is guided by Silviya maam and been worked on by the team, which is
+Hospital Management System COVID-19 and Outbreak Prediction using ML, whoch has one basic motive which 
+is to improve efficiency and simplicity of management in such difficult times of pandemic. 
+This site will eradicate use of several web/apps for functioning and put it all in one! 
+Thus making a single platform integrated with COVID Outbreak Prediction System. 
+Especially for people worried about tomorrow!
+So that was a brief introduction of the topic now, 
+
+I'll start with Problem Statement
+Hospital Management System is basically system that takes down info on 
+hospitals and their patients. It also provides various basic functions for hospital 
+staff to make management easier. Our main motive is to make a bridge between hospitals and common public.
+
+Hospital Management System is basically system that takes down info on hospitals and their patients. 
+It also provides various basic functions for hospital staff to make management 
+easier. Our main motive is to make a bridge between hospitals and common public.
+
+Now moving towards objectives behind this:
+1. administer patient’s information and admission 
+for which we'll have functions like symtoms tracker and patients records
+2. admission bills
+where we'll note down expenses and print the invoice or the bills
+3. Appointments
+The name says it, appointments will be noted down here
+4. Monitors inventory
+Noting of tools, devices, amenities etc will be carried out here
+
+Next would be Literature Survey which would be done by Suraj Tambe, over to you Suraj!
+
+
